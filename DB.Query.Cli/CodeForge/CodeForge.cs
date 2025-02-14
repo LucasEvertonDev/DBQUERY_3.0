91b94925bdd90e43603a1355490770e2eace6349
Lucas Everton Santos de Oliveira <lucas.oliveira-ext@fiven.com>
2025-02-14 11:18:32 -0300

    Upp

---
+++ DB.Query.Cli/CodeForge/CodeForge.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.Data.SqlClient;
+using System.Data;
+
+namespace DB.Query.Cli.CodeForge
+{
+    public abstract class CodeForge
+    {
+        protected string _conexao;
+        protected string _tableName;
+        protected string _database;
+        protected string _schema;
+
+        public abstract void Init();
+
+        public DataTable ExecuteQuery(string query)
+        {
+            var dataTable = new DataTable();
+            try
+            {
+                SqlCommand sqlCommand = new SqlCommand();
+                SqlConnection Sql_Conexao = OpenConnection();
+
+                SqlCommand Sql_Comando = new SqlCommand(query, Sql_Conexao);
+                SqlDataAdapter sql_Ada = new SqlDataAdapter(Sql_Comando);
+                sql_Ada.Fill(dataTable);
+            }
+            catch
+            {
+                return null;
+            }
+            return dataTable;
+        }
+
+        private SqlConnection OpenConnection()
+        {
+            SqlConnection Sql_Conexao = new SqlConnection(_conexao);
+            Sql_Conexao.Open();
+
+            if (!string.IsNullOrEmpty(_database))
+            {
+                Sql_Conexao.ChangeDatabase(_database);
+            }
+
+            return Sql_Conexao;
+        }
+    }
+}
---
+++ DB.Query.Cli/CodeForge/ForgeEntity.cs
@@ -0,0 +1,18 @@
+﻿namespace DB.Query.Cli.CodeForge
+{
+    public class ForgeEntity : CodeForge
+    {
+        public ForgeEntity(string conexao, string database, string schema, string tableName)
+        {
+            _conexao = conexao;
+            _tableName = tableName;
+            _database = database;
+            _schema = schema;
+        }
+
+        public override void Init()
+        {
+            
+        }
+    }
+}
